8f629959497f4e9f9b52ab4eff363bf248d74a9f
rachasakr <r.rachasak@gmail.com>
2022-12-23 13:23:38 +0700

    Init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="js">
+  import Paper, { Title, Content } from '@smui/paper';
+  import LayoutGrid, { Cell } from '@smui/layout-grid';
+
+</script>
+<link rel="stylesheet" href="node_modules/svelte-material-ui/bare.css" />
+<!-- Roboto -->
+<link
+  rel="stylesheet"
+  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700"
+/>
+
+<style type="text/scss">
+	@use '@material/typography/mdc-typography';
+
+	.paper-container{
+		margin-top: 35vh;
+	}
+</style>
+
+<div class="paper-container">
+	<Paper>
+		<div class="paper-wrapper">
+			<Title><h1 class="mdc-typography--headline1" style="text-align: center;">Rachasak.org</h1></Title>
+			<Content>
+				<p class="mdc-typography--body1">The <i>official, professional</i> website for Mr. Rachasak</p>
+				<small class="mdc-typography--body1">coming soon</small>
+			</Content>
+		</div>
+	</Paper>
+</div>
